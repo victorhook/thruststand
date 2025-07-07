f919d8fa9c216ef888b108ce33062a7db33d02db
Victor <victorkrook96@gmail.com>
2025-07-07 21:01:25 +0200

    Running program

---
+++ tools/device.py
@@ -0,0 +1,92 @@
+
+from serial import Serial
+from queue import Queue
+from threading import Thread, Lock
+import json
+from dataclasses import dataclass
+from argparse import Namespace, ArgumentParser
+import time
+from datetime import datetime
+import sys
+import struct
+from pathlib import Path
+
+
+@dataclass
+class Data:
+    timestamp: int
+    throttle: int
+    thrust: float
+
+class Device:
+
+    SYNC_BYTE = 0x42
+
+    def __init__(self, csv_file = None) -> None:
+        self._rx = Queue()
+        self._tx = Queue()
+        self._serial: Serial = None
+        self._data = []
+        self._lock = Lock()
+        self._latest_data = None
+        self.csv_file = csv_file
+
+    def get_latest_data(self) -> Data:
+        with self._lock:
+            return self._latest_data
+
+    def start(self, port: str, baud: int = 115200) -> None:
+        self._serial = Serial(port, baud, timeout=1, write_timeout=1)
+        Thread(target=self._rx_thread, name='Device RX', daemon=True).start()
+        Thread(target=self._tx_thread, name='Device TX', daemon=True).start()
+
+    def set_throttle(self, pwm: int) -> None:
+        self._tx.put(f'{pwm}\n')
+
+    def _rx_thread(self) -> None:
+        while True:
+            if self._serial is None:
+                break
+
+            try:
+                data = self._serial.readline()
+                timestamp, throttle, thrust = data.decode('ascii').strip().replace(' ', '').split(',')
+                new_data = Data(timestamp, throttle, thrust)
+                self._rx.put(new_data)
+                with self._lock:
+                    self._latest_data = new_data
+                if self.csv_file:
+                    self.csv_file.write(f'{timestamp},{throttle},{thrust}\n')
+            except Exception as e:
+                print(f'Error reading: {e}')
+
+        print('RX done!')
+            
+    def _tx_thread(self) -> None:
+        while True:
+            if self._serial is None:
+                break
+
+            try:
+                tx = self._tx.get()
+                if not tx.endswith('\n'):
+                    tx += '\n'
+                self._serial.write(tx.encode('ascii'))
+            except Exception as e:
+                print(f'Error reading: {e}')
+
+        print('TX done!')
+
+
+
+if __name__ == '__main__':
+    device = Device(None)
+    device.start('/dev/ttyACM0', baud=921600)
+
+    while True:
+        throttle = input('> New throttle: ')
+        try:
+            device.set_throttle(int(throttle))
+            print(f'Throttle: {device.get_latest_data().throttle}')
+        except Exception as e:
+            print(e)
--- tools/run.py
+++ tools/run.py
@@ -1,32 +1,51 @@
 #!/usr/bin/env python3
 
-from serial import Serial
-from queue import Queue
-from threading import Thread, Lock
-import json
 from dataclasses import dataclass
 from argparse import Namespace, ArgumentParser
 import time
 from datetime import datetime
-import sys
 from pathlib import Path
 from device import Device
+from pymavlink import mavutil
+import sys
 
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument('port', type=str, help='Serial port to connect to')
+    parser.add_argument('-a', '--ap', default=None, required=False, type=str, help='Ardupilot port to connect to')
     parser.add_argument('-o', '--output', type=str, default='results', help='Output directory')
+    parser.add_argument('-d', '--dry', action='store_true', default=False, help='If true, dry run, no PWM values set, only thrust measurement')
     return parser.parse_args()
 
 
-from pymavlink import mavutil
-master = mavutil.mavlink_connection('/dev/ttyACM1', baud=921600)
-print("Waiting for heartbeat...")
-master.wait_heartbeat()
+def set_motor_pwm(master: object, motor_nbr: int, pwm: int) -> None:
+    master.mav.command_long_send(
+        master.target_system,
+        master.target_component,
+        mavutil.mavlink.MAV_CMD_DO_MOTOR_TEST,
+        0,
+        motor_nbr,
+        1,
+        pwm,
+        1,
+        0, 0, 0
+    )
 
 if __name__ == '__main__':
     args = parse_args()
+
+    dryrun = args.dry
+    motor_nbr = 2
+
+    if not dryrun:
+        if args.ap is None:
+            print('Must supply AP port "--ap"')
+            sys.exit(0)
+
+        master = mavutil.mavlink_connection(args.port, baud=921600)
+        print(f'Waiting for heartbeat from AP..., using motor {motor_nbr}')
+        master.wait_heartbeat()
 
     datetime_str = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     csv_filename = f'run_{datetime_str}.csv'
@@ -36,6 +55,7 @@ if __name__ == '__main__':
 
     if not base_dir.exists():
         base_dir.mkdir(parents=True)
+
 
     with open(csv_filepath, 'w') as f:
         f.write('timestamp,pwm,thrust\n')
@@ -50,41 +70,26 @@ if __name__ == '__main__':
         pwm_min = 1000
         pwm_max = 2000
         pwm = pwm_min
-        inc_step = 1
-        delay_s = 0.1
+        inc_step = 5
+        delay_s = 20
 
         try:
             while pwm <= pwm_max:
                 pwm += inc_step
 
-                master.mav.command_long_send(
-                    master.target_system,
-                    master.target_component,
-                    mavutil.mavlink.MAV_CMD_DO_MOTOR_TEST,
-                    0,
-                    2,
-                    1,
-                    pwm,
-                    1,
-                    0, 0, 0
-                )
+                if not dryrun:
+                    set_motor_pwm(master, motor_nbr, pwm)
 
                 device.set_throttle(pwm)
                 latest = device.get_latest_data()
-                print(latest)
+
+                done = ((pwm - pwm_min) / (pwm_max - pwm_min) ) * 100
+
+                print(f'[{latest.timestamp}] {latest.throttle:3}/{pwm_max:3} ({round(done):3}%) -> {latest.thrust}')
                 time.sleep(delay_s)
         finally:
             for i in range(5):
                 device.set_throttle(1000)
-                master.mav.command_long_send(
-                    master.target_system,
-                    master.target_component,
-                    mavutil.mavlink.MAV_CMD_DO_MOTOR_TEST,
-                    0,
-                    2,
-                    1,
-                    1000,
-                    1,
-                    0, 0, 0
-                )
+                if not dryrun:
+                    set_motor_pwm(master, motor_nbr, pwm)
                 time.sleep(.05)
