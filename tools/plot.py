ce197c86b910ae677a77a4001f3cb9709394fb98
Victor <victorkrook96@gmail.com>
2025-07-11 06:58:12 +0200

    Added optimize buttons
    Not completely done, but seems to work quite nicely!

--- tools/plot.py
+++ tools/plot.py
@@ -4,6 +4,7 @@ import pandas as pd
 from argparse import ArgumentParser, Namespace
 import matplotlib.pyplot as plt
 import tkinter as tk
+from threading import Thread
 
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -11,57 +12,84 @@ from matplotlib.backends.backend_tkagg import (
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 
+
 def parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument('file', type=str, help='CSV file to plot')
     return parser.parse_args()
 
-MOT_PWM_MIN   = 1000
-MOT_PWM_MAX   = 2000
-MOT_THST_EXPO = 0.5
-MOT_SPIN_ARM  = 0.1
-MOT_SPIN_MIN  = 0.25
-MOT_SPIN_MAX  = 0.95
-
 class Gui(tk.Tk):
+
+    STYLE = {'bg': 'white'}
+    TEXT_STYLE = {'font': 'Helvetica 18'}
+    
 
     class Param:
 
-        def __init__(self, master, update_callback: callable, name: str, value: float, min_value: float, max_value: float, step: float, row: str) -> None:
-            self.master = master
-            pad = {'padx': 10, 'pady': 5}
+        def __init__(self, master, name: str, value: float, min_value: float, max_value: float, step: float, row: str, optimize: callable = None) -> None:
+            pad = {'padx': 5, 'pady': 2, 'ipadx': 2, 'ipady': 2}
             self._var = tk.StringVar()
             self._var.set(value)
-            self._var.trace_add('write', lambda *_: update_callback())
+            self.max_value = max_value
+            self.min_value = min_value
+            self.step = step
 
-            tk.Label(master, text=name).grid(row=row, column=0, **pad)
-            entry = tk.Entry(master, width=10, textvariable=self._var)
+            tk.Label(master, text=name, **Gui.TEXT_STYLE, **Gui.STYLE).grid(row=row, column=0, sticky=tk.W, **pad)
+            entry = tk.Entry(master, width=10, textvariable=self._var, **Gui.TEXT_STYLE, **Gui.STYLE)
             entry.grid(row=row, column=1, **pad)
-            tk.Scale(master, width=10, length=500, orient=tk.HORIZONTAL, variable=self._var, from_=min_value, to_=max_value, resolution=step).grid(row=row, column=2, **pad)
+            tk.Scale(master, width=10, length=600, orient=tk.HORIZONTAL, variable=self._var, from_=min_value, to_=max_value, resolution=step, **Gui.STYLE).grid(row=row, column=2, **pad)
+            if optimize:
+                tk.Button(master, text='Optimize', command=lambda: optimize(name), **Gui.TEXT_STYLE, **Gui.STYLE).grid(row=row, column=3, **pad)
+
+        def do_bind_callback(self, on_update: callable) -> None:
+            self._var.trace_add('write', lambda *_: on_update())
 
         def get(self) -> float:
             return float(self._var.get())
+        
+        def set(self, new_value: float) -> None:
+            self._var.set(new_value)
+        
+    class Label:
+
+        def __init__(self, master, name: str, value: str, row: str) -> None:
+            pad = {'padx': 5, 'pady': 2, 'ipadx': 2, 'ipady': 2}
+            self._var = tk.StringVar()
+            self._var.set(value)
+            tk.Label(master, text=name, **Gui.TEXT_STYLE, **Gui.STYLE).grid(row=row, column=0, sticky=tk.W, **pad)
+            tk.Label(master, width=10, textvariable=self._var, **Gui.TEXT_STYLE, **Gui.STYLE).grid(row=row, column=1, **pad)
+
+        def set(self, new_value: str) -> None:
+            self._var.set(new_value)
+
+        def get(self) -> str:
+            return self._var.get()
 
     def __init__(self, csv_file: str) -> None:
         super().__init__()
         self.title('Thrust curve')
-        self.geometry('800x600')
+        self.geometry('1200x800')
+        self.config(**self.STYLE)
 
         self.df = pd.read_csv(csv_file)
 
-
-        self.param_frame = tk.Frame(self)
-        self.plot_frame = tk.Frame(self)
+        self.param_frame = tk.LabelFrame(self, text='Ardupilot Parameters', **self.STYLE)
+        self.plot_frame = tk.Frame(self, **self.STYLE)
 
         # -- Parameter frame -- #
         self.params = {
-            'MOT_PWM_MIN':   self.Param(self.param_frame, self.update_plot, 'MOT_PWM_MIN',   1000, 1000, 2000, 1,    0),
-            'MOT_PWM_MAX':   self.Param(self.param_frame, self.update_plot, 'MOT_PWM_MAX',   2000, 1000, 2000, 1,    1),
-            'MOT_THST_EXPO': self.Param(self.param_frame, self.update_plot, 'MOT_THST_EXPO', 0.5,  0.0,  1.0,  0.01, 2),
-            'MOT_SPIN_ARM':  self.Param(self.param_frame, self.update_plot, 'MOT_SPIN_ARM',  0.1,  0.0,  1.0,  0.01, 3),
-            'MOT_SPIN_MIN':  self.Param(self.param_frame, self.update_plot, 'MOT_SPIN_MIN',  0.25, 0.0,  1.0,  0.01, 4),
-            'MOT_SPIN_MAX':  self.Param(self.param_frame, self.update_plot, 'MOT_SPIN_MAX',  0.95, 0.0,  1.0,  0.01, 5),
+            'MOT_PWM_MIN':   self.Param(self.param_frame, 'MOT_PWM_MIN',   1000, 1000, 2000, 1,    0, optimize=self.optimize),
+            'MOT_PWM_MAX':   self.Param(self.param_frame, 'MOT_PWM_MAX',   2000, 1000, 2000, 1,    1, optimize=self.optimize),
+            'MOT_THST_EXPO': self.Param(self.param_frame, 'MOT_THST_EXPO', 0.5,  0.0,  1.0,  0.01, 2, optimize=self.optimize),
+            'MOT_SPIN_ARM':  self.Param(self.param_frame, 'MOT_SPIN_ARM',  0.1,  0.0,  1.0,  0.01, 3),
+            'MOT_SPIN_MIN':  self.Param(self.param_frame, 'MOT_SPIN_MIN',  0.25, 0.0,  1.0,  0.01, 4, optimize=self.optimize),
+            'MOT_SPIN_MAX':  self.Param(self.param_frame, 'MOT_SPIN_MAX',  0.95, 0.0,  1.0,  0.01, 5, optimize=self.optimize),
         }
+        for p in self.params.values():
+            p.do_bind_callback(self.update_plot)
+
+        self.error_label = self.Label(self.param_frame, 'MAE', '0', 6)
+        self.mae = 0
 
         # -- Plots -- #
         self.calculate_corrected_thrust()
@@ -73,15 +101,38 @@ class Gui(tk.Tk):
 
         canvas = FigureCanvasTkAgg(self.fig, master=self.plot_frame)  # A tk.DrawingArea.
         canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        canvas.get_tk_widget().pack(side=tk.TOP, anchor=tk.N, fill=tk.BOTH, expand=1)
 
         toolbar = NavigationToolbar2Tk(canvas, self.plot_frame)
         toolbar.update()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        canvas.get_tk_widget().pack(side=tk.TOP, anchor=tk.N, fill=tk.BOTH, expand=1)
 
 
-        self.param_frame.pack(fill=tk.X, expand=True)
-        self.plot_frame.pack(fill=tk.BOTH, expand=True)
+        self.param_frame.pack(fill=tk.X, ipadx=10, ipady=10, expand=False, side=tk.TOP, anchor=tk.N)
+        self.plot_frame.pack(fill=tk.BOTH, expand=True, side=tk.TOP, anchor=tk.N)
+
+    def optimize(self, param_name: str) -> None:
+        def _optimize():
+            print(f'Optimizing parameter {param_name}')
+
+            param = self.params[param_name]
+            param.set(param.min_value)
+            
+            mae_min = float('inf')
+            param_value_at_min = 0
+
+            while param.get() < param.max_value:
+                self.calculate_corrected_thrust()
+                if self.mae < mae_min:
+                    mae_min = self.mae
+                    param_value_at_min = param.get()
+
+                param.set(param.get() + param.step)
+
+            print(f'Lowest MAE: {mae_min:.4f} at {param_name}={param_value_at_min:.2f}')
+            param.set(param_value_at_min)
+
+        Thread(target=_optimize, name='Optimizer', daemon=True).start()
 
     def update_plot(self) -> None:
         self.calculate_corrected_thrust()
@@ -109,9 +160,6 @@ class Gui(tk.Tk):
         thrust_at_MOT_SPIN_MAX = self.df.loc[(self.df['pwm'] - PWM_AT_MOT_SPIN_MAX).abs().idxmin(), 'thrust']
         self.df['voltage'] = 8
 
-        print(thrust_at_MOT_SPIN_MIN)
-        print(thrust_at_MOT_SPIN_MAX)
-
         normalized_throttle = (self.df['voltage'] / self.df['voltage'].max()) * ( (self.df['pwm']-PWM_AT_MOT_SPIN_MIN)/(PWM_AT_MOT_SPIN_MAX-PWM_AT_MOT_SPIN_MIN) ).clip(lower=0)
         normalized_thrust = ((1 - MOT_THST_EXPO) * normalized_throttle + MOT_THST_EXPO*normalized_throttle*normalized_throttle).clip(lower=0)
         corrected_thrust = normalized_thrust * thrust_at_MOT_SPIN_MAX + thrust_at_MOT_SPIN_MIN
@@ -120,6 +168,11 @@ class Gui(tk.Tk):
         self.df['normalized_thrust'] = normalized_thrust
         self.df['corrected_thrust'] = corrected_thrust
 
+        self.mae = self.evaluate_fit()
+        self.error_label.set(f'{self.mae:.4f}')
+
+    def evaluate_fit(self) -> float:
+        return (self.df['thrust'] - self.df['corrected_thrust']).abs().mean()
 
 
 # -- Normalized Throttle
@@ -145,11 +198,3 @@ if __name__ == '__main__':
 
     gui = Gui(args.file)
     gui.mainloop()
-
-    #plt.plot(df['normalized_throttle'], df['thrust'], label='Measured thrust', color='red')
-    #plt.plot(df['normalized_throttle'], df['corrected_thrust'], label='Corrected thrust', color='blue')
-    #plt.xlabel('Normalized throttle [0-1]')
-    #plt.ylabel('Thrust [kgf]')
-    #plt.legend()
-    #plt.show()
-
